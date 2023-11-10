e855943a2ba874d102dae9d038ace3abfeeac33d
Jason Kulak <JasonKulak@yahoo.com>
2023-11-09 21:54:30 -0600

    Test

---
+++ answers.js
@@ -0,0 +1,36 @@
+////////////////////
+//PART 1: HTML & CSS
+////////////////////
+
+
+
+////////
+//PART 2
+////////
+
+////////////
+//EASY GOING
+////////////
+
+for (let i = 1; i <= 20; i++) {
+    console.log(i)
+}
+
+//////////
+//Get Even
+//////////
+
+for (let i = 2; i <= 200; i+=2) {
+    console.log(i)
+}
+
+///////////
+//Fizz Buzz
+///////////
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i)
+if (i % 3 === 0){
+    console.log('Fizz')
+}
+}
